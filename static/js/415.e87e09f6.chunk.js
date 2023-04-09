cad156b0a65935acd035d573379997d387678241
MaxF1996 <98663185+MaxF1996@users.noreply.github.com>
2023-04-09 15:40:27 +0000

    Deploying to gh-pages from @ MaxF1996/goit-react-hw-05-movies@bb9cf698e3d115ed4f0f9a80d747fbe11ba550f3 🚀

---
+++ static/js/415.e87e09f6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[415],{415:function(e,n,s){s.r(n);var t=s(791),i=s(793),r=s(184),u=(0,t.lazy)((function(){return Promise.all([s.e(279),s.e(191)]).then(s.bind(s,191))}));n.default=function(){return(0,r.jsxs)(t.Suspense,{fallback:(0,r.jsx)(i.Z,{}),children:[(0,r.jsx)(u,{}),";"]})}}}]);
+//# sourceMappingURL=415.e87e09f6.chunk.js.map
